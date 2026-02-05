d90e3122a588abbeb82bd70b374ba3c5e9713b94
Anselme <anselmedupuy@gmail.com>
2026-02-05 10:46:16 +0100

    feat: 0.0.2 init project - modif BDD

--- apps/models/order.py
+++ apps/models/order.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Order(models.Model):
-    user_id = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=[('PENDING', 'Pending'), ('SHIPPED', 'Shipped'), ('PAID', 'Paid')], default='PENDING')
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
--- apps/models/orderItem.py
+++ apps/models/orderItem.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class OrderItem(models.Model):
-    order_id = models.ForeignKey('Order', on_delete=models.CASCADE)
-    product_id = models.ForeignKey('Product', on_delete=models.CASCADE)
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     unit_price_frozen = models.DecimalField(max_digits=10, decimal_places=2)
     
